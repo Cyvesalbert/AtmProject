ae2c4857fc32b344947291cb31cf55b2ee5e0628
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-07-30 21:53:17 -0400

    Creating class withdrawal

--- AtmProject/src/ht/atm/Withdrawal.java
+++ AtmProject/src/ht/atm/Withdrawal.java
@@ -1,5 +1,27 @@
 package ht.atm;
 
-public class Withdrawal {
-
+// Represents an ATM withdrawal transaction
+public class Withdrawal extends Transaction{
+	
+	// Attributes
+	//private int accountNumber; // account to withdraw funds from
+	private double amount; // amount to withdraw
+	
+	// References to associated objects
+	//private Screen screen; // ATM's screen
+	private Keypad keypad; // ATM's keypad
+	private CashDispenser cashDispenser; // ATM's cash dispenser
+	//private BankDatabase bankDatabase; // account info database
+	
+	// no arguments constructor
+	public Withdrawal() {
+		
+	}
+	
+	//operations
+	@Override
+	public void execute() {
+		
+	}
+	
 }
