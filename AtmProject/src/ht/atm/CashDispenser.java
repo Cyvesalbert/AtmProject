e40dc2a547010b7df50829206e6d71919769bbf3
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-08-01 23:58:56 -0400

    Creating class cash dispenser of the ATM

---
+++ AtmProject/src/ht/atm/CashDispenser.java
@@ -0,0 +1,13 @@
+package ht.atm;
+
+// represents the cash dispenser of the ATM
+public class CashDispenser {
+
+	private final static int INITIAL_COUNT = 500; // the default initial number of bills in the cash dispenser
+	private int count; // number of $20 bills remaining
+	
+	
+	
+	
+	
+}
